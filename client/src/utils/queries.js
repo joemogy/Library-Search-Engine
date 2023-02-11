2db39035934405fecd2648273404e475c794f719
Joseph Mogavero <82268976+joemogy@users.noreply.github.com>
2023-02-11 11:51:33 -0500

    Update queries.js

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,21 +1,19 @@
 import { gql } from '@apollo/client';
 
-export const QUERY_USER = gql`
-  query allProfiles {
-    profiles {
+export const GET_ME = gql`
+  query Query {
+    me {
       _id
-      name
-      skills
-    }
-  }
-`;
-
-export const QUERY_SINGLE_PROFILE = gql`
-  query singleProfile($profileId: ID!) {
-    profile(profileId: $profileId) {
-      _id
-      name
-      skills
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
     }
   }
 `;
